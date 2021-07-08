9a2c21c863a53e5b85f97a3c981899d009918836
Paargav Shanker <paargav.shuttle@gmail.com>
2021-07-08 14:29:32 +0530

    added projects view

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -2,8 +2,9 @@ import styles from "./Projects.module.css";
 import SectionHeader from "../../components/SectionHeader/SectionHeader";
 import { PROJECTS } from "../../data/Projects";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
-
-import React, { useRef, useEffect } from "react";
+import { MdViewCarousel } from "react-icons/md";
+import { BsGrid3X3GapFill } from "react-icons/bs";
+import React, { useRef, useEffect, useState } from "react";
 import { gsap, Power3 } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
@@ -11,9 +12,14 @@ gsap.registerPlugin(ScrollTrigger);
 function Projects() {
   let sectionRef = useRef(null);
   let projectsRef = new Array(PROJECTS.length);
+  let contentWrapperRef = useRef(null);
+
+  const [projectsView, setprojectsView] = useState("horizontal");
+
+  let timeline = gsap.timeline();
 
   useEffect(() => {
-    const timeline = gsap.timeline();
+    contentWrapperRef.current.classList.add(`${styles.horizontal_content}`);
 
     for (let i = 0; i < projectsRef.length; i++) {
       timeline.from(projectsRef[i], {
@@ -28,17 +34,50 @@ function Projects() {
       trigger: sectionRef.current,
       animation: timeline,
       start: "top 80%",
-      end: "center 70%",
+      end: "300px 65%",
       scrub: 0.5,
     });
-
     // eslint-disable-next-line
   }, []);
+
+  const changeProjectsView = (view) => {
+    setprojectsView(view);
+    if (view === "horizontal") {
+      contentWrapperRef.current.classList.remove(`${styles.grid_content}`);
+      contentWrapperRef.current.classList.add(`${styles.horizontal_content}`);
+    } else if (view === "grid") {
+      contentWrapperRef.current.classList.remove(
+        `${styles.horizontal_content}`
+      );
+      contentWrapperRef.current.classList.add(`${styles.grid_content}`);
+    }
+  };
 
   return (
     <section name="projects" ref={sectionRef} className={`${styles.wrapper}`}>
       <SectionHeader name={"projects"} />
-      <div className={`${styles.content_wrapper} ${styles.scrollbar}`}>
+      <div className={`${styles.view_container}`}>
+        <span>
+          <MdViewCarousel
+            className={`${styles.view_icon}`}
+            onClick={() => changeProjectsView("horizontal")}
+            style={{
+              color: projectsView === "horizontal" && "var(--secondary)",
+            }}
+          />
+        </span>
+        <span>
+          <BsGrid3X3GapFill
+            className={`${styles.view_icon}`}
+            onClick={() => changeProjectsView("grid")}
+            style={{
+              color: projectsView === "grid" && "var(--secondary)",
+            }}
+          />
+        </span>
+      </div>
+
+      <div ref={contentWrapperRef} className={`${styles.content_wrapper}`}>
         {PROJECTS.map((project, key) => {
           return (
             <ProjectCard
