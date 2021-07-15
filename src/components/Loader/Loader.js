68c6a40d413169bf8e8a188b4edf5e31b14c3ff3
Paargav Shanker <paargav.shuttle@gmail.com>
2021-07-15 22:12:36 +0530

    added new loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -3,7 +3,8 @@ import styles from "./Loader.module.css";
 function Loader({ message }) {
   return (
     <div className={`${styles.wrapper}`}>
-      <div className={`${styles.bouncer}`}>
+      <div className={`${styles.loader}`}>
+        <div></div>
         <div></div>
         <div></div>
         <div></div>
