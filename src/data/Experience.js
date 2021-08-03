19645947d0fd9709fcb645f77bfa64c2b857ad73
Paargav Shanker <paargav.shuttle@gmail.com>
2021-08-03 21:07:04 +0530

    minor updates

--- src/data/Experience.js
+++ src/data/Experience.js
@@ -1,21 +1,7 @@
 export const EXPERIENCES = [
   {
-    organization: "Dumbel",
-    logo: "dumbel.png",
-    duration: "Jun '21 - Present",
-    role: "Mobile App Developer",
-    workedIn: "Flutter",
-  },
-  {
-    organization: "Computer Society of Anna University",
-    logo: "csau.png",
-    duration: "Jun '21 - Present",
-    role: "Student Director (Web and App)",
-    workedIn: "Angular, Bootstrap",
-  },
-  {
     organization: "Totallr",
-    logo: "company.png",
+    logo: "totallr.png",
     duration: "Apr '21 - Present",
     role: "Web Developer Intern",
     workedIn: "Angular, Spring Boot, AWS S3",
@@ -33,5 +19,12 @@ export const EXPERIENCES = [
     duration: "Jan '21 - Apr '21",
     role: "Organizer and Frontend Developer",
     workedIn: "React",
+  },
+  {
+    organization: "Computer Society of Anna University",
+    logo: "csau.png",
+    duration: "Aug '20 - Present",
+    role: "Student Director (Web and App)",
+    workedIn: "Angular, Bootstrap",
   },
 ];
--- src/data/Resume.js
+++ src/data/Resume.js
@@ -1,2 +1,2 @@
 export const RESUME =
-  "https://drive.google.com/file/d/1yopVOPKdOKtesOojV34OJMVsNHUDu-u5/view?usp=sharing";
+  "https://drive.google.com/file/d/1MWKJWw3-7itI7PZCcBZLcCxXIwJJffaG/view?usp=sharing";
