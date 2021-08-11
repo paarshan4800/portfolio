87aa0275b6ccb3e9d6553fa0707a634d903bb582
Paargav Shanker <paargav.shuttle@gmail.com>
2021-08-11 23:07:44 +0530

    minor changes
    removed google fonts. added base64 fonts.

--- src/data/Experience.js
+++ src/data/Experience.js
@@ -1,23 +1,23 @@
 export const EXPERIENCES = [
   {
-    organization: "Totallr",
+    organization: "Totallr Technologies",
     logo: "totallr.png",
-    duration: "Apr '21 - Present",
-    role: "Web Developer Intern",
+    duration: "Apr '21 - Jul '21",
+    role: "Full Stack Web Developer",
     workedIn: "Angular, Spring Boot, AWS S3",
-  },
-  {
-    organization: "Computer Science and Engineering Association",
-    logo: "csea.png",
-    duration: "Jan '21 - Mar '21",
-    role: "Frontend Developer",
-    workedIn: "React",
   },
   {
     organization: "CEG Tech Forum",
     logo: "ctf.png",
     duration: "Jan '21 - Apr '21",
     role: "Organizer and Frontend Developer",
+    workedIn: "React",
+  },
+  {
+    organization: "Computer Science and Engineering Association, CEG",
+    logo: "csea.png",
+    duration: "Jan '21 - Mar '21",
+    role: "Frontend Developer",
     workedIn: "React",
   },
   {
